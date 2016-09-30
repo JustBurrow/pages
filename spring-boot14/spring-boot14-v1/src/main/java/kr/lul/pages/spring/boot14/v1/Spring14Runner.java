dc5c63a3becd0b278c23890f05981ebb1cd316e7
Just Burrow <just.burrow@lul.kr>
2016-10-01 00:32:04 +0900

    (INIT) Spring Boot 1.4
    Spring Boot 1.4.1 최소 코드.
    기능은 없고, `@SpringBootApplication`을 사용한 설정 테스트.

---
+++ spring-boot14/spring-boot14-v1/src/main/java/kr/lul/pages/spring/boot14/v1/Spring14Runner.java
@@ -0,0 +1,21 @@
+package kr.lul.pages.spring.boot14.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Just Burrow
+ * @since 2016. 10. 1.
+ */
+@SpringBootApplication
+public class Spring14Runner {
+  /**
+   * @param args
+   * @throws Exception
+   * @author Just Burrow
+   * @since 2016. 10. 1.
+   */
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Spring14Runner.class, args);
+  }
+}
